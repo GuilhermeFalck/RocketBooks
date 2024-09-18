a9f6a58117dddf5e24e039c797616d87fb69db2a
guilherme <guilhermefalck22@gmail.com>
2024-09-18 11:33:14 -0300

    add component buitton

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,9 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/global";
+import theme from "./styles/theme";
+
+import { Details } from "./pages/Details";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Details />
+    </ThemeProvider>
   </StrictMode>
 );
---
+++ Frontend/src/pages/Details/index.jsx
@@ -0,0 +1,16 @@
+import { Container } from "./styles";
+
+import { Button } from "../../components/Button";
+
+export function Details() {
+  return (
+    <Container>
+      <h1>Hello World!</h1>
+      <span>Guilherme Falck</span>
+
+      <Button title="Entrar" loading />
+      <Button title="Cadastrar" />
+      <Button title="Voltar" />
+    </Container>
+  );
+}
